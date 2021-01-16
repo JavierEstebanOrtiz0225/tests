503ed85596367cb1af13a1f84be8231c23f9c10f
Javier <jeortiz19@misena.edu.co>
2021-01-16 00:17:48 -0500

    [add] index

---
+++ generator/index.js
@@ -0,0 +1,7 @@
+let fs = require('fs')
+
+let json = {"name":"hola","type":"prueba"}
+for (let index = 0; index < 10; index++) {
+    fs.writeFileSync(`../files/${index}.json`,JSON.stringify(json))
+    
+}
